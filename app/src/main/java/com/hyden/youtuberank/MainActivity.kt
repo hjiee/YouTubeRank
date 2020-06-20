c123b578259a4f7826c55503f11954e172ffe0d6
hyojin <jin33032@gmail.com>
2020-06-20 23:36:01 +0900

    ADD [ 초기 화면 세팅 ]

--- app/src/main/java/com/hyden/youtuberank/MainActivity.kt
+++ app/src/main/java/com/hyden/youtuberank/MainActivity.kt
@@ -2,11 +2,12 @@ package com.hyden.youtuberank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyden.base.BaseActivity
+import com.hyden.youtuberank.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
